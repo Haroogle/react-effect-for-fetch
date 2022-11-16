efe1633f25a5c2eb9e68bb75a39b276e5e28c90e
Haroun Anwari <harounanwari@hotmail.co.uk>
2022-11-16 17:58:05 +0000

    Arts and users done, users to be refactored

--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,12 +1,24 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import ArtList from "./components/ArtList";
 
 function ArtsSection() {
+  const [artData, setArtData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.artic.edu/api/v1/artworks?page=1&limit=10")
+      .then((res) => res.json())
+      .then((gallery) => setArtData(gallery.data));
+  }, []);
+  console.log(artData);
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        {/* show list of art here*/}
+        <ArtList artData={artData} />
+      </div>
     </section>
-  )
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
--- src/sections/Users/index.js
+++ src/sections/Users/index.js
@@ -1,12 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function UsersSection() {
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+      .then((res) => res.json())
+      .then((data) => setUserData(data.results));
+  }, []);
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          {userData.map((user, index) => {
+            const gender = user.gender === "female";
+            return (
+              <li key={index} className={gender ? "bg-pink" : "bg-blue"}>
+                <img src={`${user.picture.medium}`} alt={`${user.name}`} />
+                <h3>
+                  {user.name.title} {user.name.first} {user.name.last}
+                </h3>
+                <p>{user.email}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
-  )
+  );
 }
 
-export default UsersSection
+export default UsersSection;
